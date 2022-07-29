a036a8dfa7ae7e76a03990e2244b3a3cd618a29b
phonokoye <78999501+phonokoye@users.noreply.github.com>
2022-07-29 04:18:41 +0100

    Add files via upload

---
+++ Day 11/Assignment.js
@@ -0,0 +1,3 @@
+// A command that will print out elements at index 0,
+// at index of 'love'
+// substring of 'c'
---
+++ Day 11/index.js
@@ -0,0 +1,12 @@
+// document.write
+// alert
+// console.log
+
+document.write('<h1>FrontEnd Class</h1>')
+document.write('<hr>')
+document.write('<h1>I Love Verbum Networks!</h1>')
+
+// alert('<h1>I Love Verbum Networks!</h1>')
+console.log('I Love Verbum Networks!')
+
+document.write(100 + parseInt('25') + '<br>')
